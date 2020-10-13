105dccef45eeeaf16cb840e2f2728724e9edba9d
looka <looka@rpi.edu>
2020-10-13 10:01:13 -0400

    building?

--- Labs/lab5/build.sh
+++ Labs/lab5/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 -m grpc_tools.protoc -I../grpc/protos --python_out=. --grpc_python_out=. ./route_guide.proto
+python3 -m grpc_tools.protoc -I./ --python_out=. --grpc_python_out=. ./route_guide.proto
