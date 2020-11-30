6ae430a711ac26d652146263feb360db58bd6273
2042Third <18604713262@163.com>
2020-11-30 16:38:05 -0500

    base code

---
+++ Homework/HW4/hw4_client.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import sys  # For arg parsing
+import socket  # For sockets
+
+def run_client():
+    if len(sys.argv) != 3:
+        print(f"Proper usage is {sys.argv[0]} [server name/address] [server port]")
+        sys.exit(0)
+
+    # Create the TCP socket, connect to the server
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # bind takes a 2-tuple, not 2 arguments
+    server_socket.connect((sys.argv[1], int(sys.argv[2])))
+
+    while True:
+        # Read a string from standard input
+        send_string = input("Enter a string to send: ")
+
+        # Alternate method
+        '''
+        print("Enter a string to send: ")
+        send_string = sys.stdin.readline()
+        '''
+
+        # Send the message to the server, even if it takes multiple send() calls
+        # Could just use send(), for small messages that should be fine
+        # We need to "encode" to make this bytes, technically a string str() is NOT
+        server_socket.sendall(send_string.encode('utf-8'))
+        if not send_string:
+            # Disconnect from the server
+
+            print("Closing connection to server")
+            server_socket.close()
+            break
+        # Get the response from the server
+        recv_string = server_socket.recv(1024)
+
+        
+
+        # Print the response to standard output, both as byte stream and decoded text
+        print(f"Received {recv_string} from the server")
+        print(f"Decoding, received {recv_string.decode('utf-8')} from the server")
+
+        
+
+    
+
+if __name__ == '__main__':
+    run_client()
---
+++ Homework/HW4/hw4_server.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import sys  # For arg parsing
+import socket  # For sockets
+import select, queue
+
+def run_server():
+    if len(sys.argv) != 2:
+        print(f"Proper usage is {sys.argv[0]} [port number]")
+        sys.exit(0)
+
+    # Create a TCP socket
+    listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Set the socket to listen on any address, on the specified port
+    # bind takes a 2-tuple, not 2 arguments
+    listening_socket.bind(('', int(sys.argv[1])))
+    listening_socket.listen(5)
+    
+    #read set
+    rset = [listening_socket]
+    #write set
+    wset = []
+    #msg queue (dictionary)
+    mque = {}
+
+    # Server loop
+    while rset: 
+        # readable, writable, exceptional
+        rable, wable, excp = select.select(rset, wset, rset)
+        
+        for sock in rable:
+            if sock is listening_socket:
+                (client_socket, address) = sock.accept()
+                client_socket.setblocking(0)
+                rset.append(client_socket)
+                mque[client_socket] = queue.Queue()
+            else:
+                message = sock.recv(1024)
+                if message:
+                    mque[sock].put(message)
+
+                    print(f"Server received {len(message)} bytes: \"{message}\"")
+                    if sock not in wset:
+                        wset.append(sock)
+                else:
+                    if sock in wset:
+                        wset.remove(sock)
+                    rset.remove(sock)
+                    sock.close()
+                    del mque[sock]
+
+        for sock in wable:
+            try:
+                next_msg = mque[sock].get_nowait()
+            except :
+                wset.remove(sock)
+            else:
+                print(f"Server sending {len(next_msg)} bytes: \"{next_msg}\"")
+                sock.send(next_msg)
+
+        for sock in excp:
+            rable.remove(sock)
+            if sock in wset:
+                wset.remove(sock)
+            sock.close()
+            del mque[sock]
+
+
+
+if __name__ == '__main__':
+    run_server()
